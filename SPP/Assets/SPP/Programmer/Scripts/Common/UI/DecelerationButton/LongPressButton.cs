ae7e8ace0768dd7b5c0c313351346b193fca4bd5
Hiroto Morikawa <somnium2p@gmail.com>
2017-01-13 16:15:34 +0900

    長押しでTRUE，離すとFALSEを返すボタンを作成

---
+++ SPP/Assets/SPP/Programmer/Scripts/Common/UI/DecelerationButton/LongPressButton.cs
@@ -0,0 +1,44 @@
+﻿/*************************************************************
+@file       LongPressButton.cs
+@brief      押している間にTRUE，その他はFALSEを返す
+@author     Hiroto Morikawa
+
+@Update
+    2017/1/13   LongPressButton.csを作成
+                機能の完成
+
+Copyright © 2017 Hiroto Morikawa All Rights Reserved.
+***************************************************************/
+using UnityEngine;
+using System.Collections;
+
+public class LongPressButton : BaseObject {
+
+    [SerializeField]
+    private bool m_isPress;
+
+    /*********************
+    @brief      プロパティ
+    */
+    public bool mIsPress {
+
+        get;
+        private set;
+    }
+
+    /*****************************************
+    @brief      ボタンを押している間TRUEとなる
+    */
+    public void mPushButton()
+    {
+        mIsPress = m_isPress = true;
+    }
+
+    /********************************************
+    @brief       ボタンを押していない間FALSEとなる
+    */
+    public void mPullButton()
+    {
+        mIsPress = m_isPress = false;
+    }
+}
