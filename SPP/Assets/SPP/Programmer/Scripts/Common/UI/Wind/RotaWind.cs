ed470c926932ac96542ccd06f4b5ae9961211fc2
yutatakatsu <vocaloid_asax@yahoo.co.jp>
2017-01-21 01:27:06 +0900

    風のレーダー上げなおし

---
+++ SPP/Assets/SPP/Programmer/Scripts/Common/UI/Wind/RotaWind.cs
@@ -0,0 +1,24 @@
+﻿/*************************************************************
+@file       RotaWind.cs
+@brief      オブジェクトの軸となる存在 
+            gameobjectを中心にその周りを回転させる
+
+@author     yuta takatsu
+
+***************************************************************/
+using UnityEngine;
+using System.Collections;
+
+public class RotaWind : BaseObject
+{
+
+    public float m_gizmoSize = 0.3f;
+    public Color m_gizmoColor = Color.yellow;
+
+    void OnDrawGizmos()
+    {
+
+        Gizmos.color = m_gizmoColor;
+        Gizmos.DrawWireSphere(transform.position, m_gizmoSize);
+    }
+}
--- SPP/Assets/SPP/Programmer/Scripts/Common/UI/Wind/UIWind.cs
+++ SPP/Assets/SPP/Programmer/Scripts/Common/UI/Wind/UIWind.cs
@@ -5,18 +5,38 @@ using UnityEngine.UI;
 
 public class UIWind : BaseObject
 {
+    //マテリアル関連
+    public Material mat;
+    private Color color;
 
     [SerializeField]
     private WindObject m_wind;
     [SerializeField]
     private ShipMove m_ship;
+   
 
     public override void mOnUpdate()
     {
-       
-        base.mOnUpdate();
 
+        base.mOnUpdate();
+        Debug.Log(m_ship.mSpeedVector / m_ship.mMaxSpeed);
         //m_WindDirectionの値分回転
-        transform.localEulerAngles = new Vector3(0, m_wind.mWindDirection - m_ship.transform.eulerAngles.y, 0);
+        transform.eulerAngles = new Vector3(0, 0, m_ship.transform.eulerAngles.y - m_wind.mWindDirection);
+
+        //風向きが-25～25の値の時UIの色を赤に変える それ以外は緑
+        if (m_ship.transform.eulerAngles.y - m_wind.mWindDirection >= -25.0 &&
+           m_ship.transform.eulerAngles.y - m_wind.mWindDirection <= 25.0)
+        //if (m_ship.mSpeedVector / m_ship.mMaxSpeed <= 0)
+        {
+            //赤
+            color = new Color(1.0F, 0.0F, 0.0F, 1.0F);
+        }
+        else
+        {
+            //緑
+            color = new Color(0.0F, 1.0f, 0.0F, 1.0F);
+        }
+        //色を変更
+        mat.SetColor("_Color", color);
     }
 }
