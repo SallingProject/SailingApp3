3768653078269d6c8d55dce2fb9ec2f01cf526b9
yutatakatsu <vocaloid_asax@yahoo.co.jp>
2017-01-25 11:02:13 +0900

    風のレーダー用スクリプト再アップ

--- SPP/Assets/SPP/Programmer/Scripts/Common/UI/Wind/UIWind.cs
+++ SPP/Assets/SPP/Programmer/Scripts/Common/UI/Wind/UIWind.cs
@@ -5,28 +5,37 @@ using UnityEngine.UI;
 
 public class UIWind : BaseObject
 {
+
+    static readonly float m_kCanntMoveAngle = 25;
+
     //マテリアル関連
     public Material mat;
     private Color color;
+
 
     [SerializeField]
     private WindObject m_wind;
     [SerializeField]
     private ShipMove m_ship;
-   
+
 
     public override void mOnUpdate()
     {
 
         base.mOnUpdate();
-        Debug.Log(m_ship.mSpeedVector / m_ship.mMaxSpeed);
+
+
         //m_WindDirectionの値分回転
         transform.eulerAngles = new Vector3(0, 0, m_ship.transform.eulerAngles.y - m_wind.mWindDirection);
 
+
+        float windDirection = m_ship.transform.eulerAngles.y - m_wind.mWindDirection;
+        float typeMinus = m_ship.transform.eulerAngles.y - m_kCanntMoveAngle;
+
         //風向きが-25～25の値の時UIの色を赤に変える それ以外は緑
-        if (m_ship.transform.eulerAngles.y - m_wind.mWindDirection >= -25.0 &&
-           m_ship.transform.eulerAngles.y - m_wind.mWindDirection <= 25.0)
-        //if (m_ship.mSpeedVector / m_ship.mMaxSpeed <= 0)
+
+        if (m_ship.transform.eulerAngles.y - m_wind.mWindDirection <= -25.0 &&
+            m_ship.transform.eulerAngles.y - m_wind.mWindDirection <= 25.0)
         {
             //赤
             color = new Color(1.0F, 0.0F, 0.0F, 1.0F);
